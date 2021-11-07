7b830b87f14e08c7c524e603c8387cfe15eb9784
hyewon <nohye12@gmail.com>
2021-11-08 01:35:32 +0900

    [vstagram] pwa 설정

---
+++ docs/precache-manifest.f54d36db26a9795934a29b1af8bb110f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10601e285e6558da113e",
+    "url": "/vstagram/css/app.5454e4a1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vstagram/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3b1e84692bec1c9dc67485ec7fc143bc",
+    "url": "/vstagram/index.html"
+  },
+  {
+    "revision": "10601e285e6558da113e",
+    "url": "/vstagram/js/app.cc3e5857.js"
+  },
+  {
+    "revision": "5048d0e098d8f18d34f9",
+    "url": "/vstagram/js/chunk-vendors.b391a23f.js"
+  },
+  {
+    "revision": "e2e9b3f5b373d3c83de556a73a3ce5d0",
+    "url": "/vstagram/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vstagram/robots.txt"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./assets/store";
+import './registerServiceWorker'
 // import mitt from "mitt";
 // let emitter = mitt();
 let app = createApp(App);
